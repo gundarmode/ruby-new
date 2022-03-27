dd78f8abf7082fc7130f4e8a1687d44c84b3de34
gundarmode <jin_masenko@hotmail.com>
2022-03-27 14:11:49 +0100

    changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,27 @@
 import dynamic from 'next/dynamic'
+import {useState} from 'react'
 
 
 
  const Navigation = dynamic(() => import('./Navigation'))
 
 const Layout = ({children, windowWidth, windowHeight, display, router}) => {
+
+	const[loading, setLoading] = useState(true)
+
+	setTimeout(() => {
+		setLoading(false)
+	}, 1000);
+
   return (
-    <>
-    <Navigation windowWidth={windowWidth} windowHeight={windowHeight} display={display} router={router} />
-    <main>{children}</main>
-    </>
+	<>
+	{
+
+		loading ?	null : <Navigation windowWidth={windowWidth} windowHeight={windowHeight} display={display} router={router} />
+	}
+	
+	<main>{children}</main>
+	</>
   )
 }
 
