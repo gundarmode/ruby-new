218a081b244bc6e01abe3da17a847d0c56584cb3
gundarmode <jin_masenko@hotmail.com>
2022-03-27 14:08:01 +0100

    changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import Navigation from './Navigation'
+import dynamic from 'next/dynamic'
+
+
+
+ const Navigation = dynamic(() => import('./Navigation'))
 
 const Layout = ({children, windowWidth, windowHeight, display, router}) => {
   return (
     <>
-    <main>{children}</main>
     <Navigation windowWidth={windowWidth} windowHeight={windowHeight} display={display} router={router} />
+    <main>{children}</main>
     </>
   )
 }
